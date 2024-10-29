726cddc96afba022811955a25de73f5cd5fbe96f
Asher Leffler <asher.leffler@gmail.com>
2024-10-29 15:15:19 -0400

    Add turbo button to teleopPeriodic

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -19,14 +19,17 @@ import edu.wpi.first.util.sendable.SendableRegistry;
 import frc.robot.simulation.SparkMaxWrapper;
 
 /**
- * This is a demo program showing the use of the DifferentialDrive class. Runs the motors with
+ * This is a demo program showing the use of the DifferentialDrive class. Runs
+ * the motors with
  * arcade steering.
  */
 public class Robot extends TimedRobot {
-  //private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
-  //private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
-  //private final CANSparkMax m_leftMotor= new CANSparkMax(1, MotorType.kBrushed);
-  //private final CANSparkMax m_rightMotor= new CANSparkMax(2, MotorType.kBrushed);
+  // private final PWMSparkMax m_leftMotor = new PWMSparkMax(0);
+  // private final PWMSparkMax m_rightMotor = new PWMSparkMax(1);
+  // private final CANSparkMax m_leftMotor= new CANSparkMax(1,
+  // MotorType.kBrushed);
+  // private final CANSparkMax m_rightMotor= new CANSparkMax(2,
+  // MotorType.kBrushed);
 
   MotorController m_frontLeft = new SparkMaxWrapper(2, MotorType.kBrushed);
   MotorController m_rearLeft = new SparkMaxWrapper(4, MotorType.kBrushed);
@@ -35,10 +38,9 @@ public class Robot extends TimedRobot {
   MotorController m_frontRight = new SparkMaxWrapper(1, MotorType.kBrushed);
   MotorController m_rearRight = new SparkMaxWrapper(3, MotorType.kBrushed);
   MotorControllerGroup m_right = new MotorControllerGroup(m_frontRight, m_rearRight);
-  
+
   private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_left, m_right);
   private final XboxController m_controller = new XboxController(0);
-
 
   /**
    * This method is run when the robot is first started up and should be
@@ -46,19 +48,21 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    //These methods give names to our shuffleboard livewindow outputs
+    // These methods give names to our shuffleboard livewindow outputs
     SendableRegistry.add(m_robotDrive, "drive");
     SendableRegistry.add(m_left, "left");
     SendableRegistry.add(m_right, "right");
-   }
-
-
+  }
 
   @Override
   public void teleopPeriodic() {
     // Drive with arcade drive.
     // That means that the Y axis drives forward
     // and backward, and the X turns left and right.
-    m_robotDrive.arcadeDrive(m_controller.getLeftY(), m_controller.getLeftX());
+    if (m_controller.getLeftBumperPressed()) {
+      m_robotDrive.arcadeDrive(m_controller.getLeftY() * 2, m_controller.getRightY() * 2);
+    } else {
+      m_robotDrive.arcadeDrive(m_controller.getLeftY(), m_controller.getLeftX());
+    }
   }
 }
